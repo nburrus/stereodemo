8033e945cd845f37132ff9b241c4b50c791b9bee
Nicolas Burrus <nicolas@burrus.name>
2022-05-16 13:48:05 +0200

    Make the UI decent, add all parameters to StereoBM/SGBM.

--- stereodemo/methods.py
+++ stereodemo/methods.py
@@ -3,7 +3,7 @@
 from abc import abstractmethod
 from dataclasses import dataclass
 
-from typing import Dict, List
+from typing import Any, Dict, List
 
 import numpy as np
 import cv2
@@ -14,12 +14,34 @@ class IntParameter:
     value: int
     min: int
     max: int
+    to_valid: Any = lambda x: x # default is to just accept anything
+
+    def set_value (self, x: int):
+        self.value = self.to_valid(x)
+
+@dataclass
+class EnumParameter:
+    description: str
+    index: int # index in the list
+    values: List[str]
+
+    def set_index (self, idx: int):
+        self.index = idx
+
+def odd_only(x):
+    return x if x % 2 == 1 else x+1
+
+def multiple_of_16(x):
+    return max(16, x//16 * 16)
 
 class StereoMethod:
     def __init__(self, name: str, description: str, parameters: Dict):
         self.name = name
         self.parameters = parameters
         self.description = description
+
+    def reset_defaults(self):
+        pass
 
     @abstractmethod    
     def compute_disparity(self, left_image: np.ndarray, right_image: np.ndarray) -> np.ndarray:
@@ -29,14 +51,27 @@ class StereoMethod:
         """
         pass
 
+# Default parameters taken from 
+# https://github.com/opencv/opencv/blob/4.x/samples/cpp/stereo_match.cpp
 class StereoBMMethod(StereoMethod):
     def __init__(self):
-        params = {
-            "Num Disparities": IntParameter("Number of disparities", 64, 2, 256),
-            "Block Size": IntParameter("Kernel size for block matching", 15, 3, 63)
-        }
+        super().__init__("OpenCV BM", "OpenCV Simple Block Matching", {})
+        self.reset_defaults()
 
-        super().__init__("BM", "OpenCV Simple Block Matching", params)
+    def reset_defaults(self):
+        # For more details:
+        # https://learnopencv.com/depth-perception-using-stereo-camera-python-c/
+        self.parameters.update ({
+            "Num Disparities": IntParameter("Number of disparities (pixels)", 64, 16, 256, to_valid=multiple_of_16),
+            "Block Size": IntParameter("Kernel size for block matching (odd)", 9, 3, 63, to_valid=odd_only),
+            "TextureThreshold": IntParameter("Minimum SAD to consider the texture sufficient", 10, 0, 100),
+            "Uniqueness Ratio": IntParameter("How unique the match each for each pixel", 15, 0, 100),
+            "SpeckleWindowSize": IntParameter("Speckle window size in pixels (filter small objects). 0 to disable.", 100, 0, 1000),
+            "SpeckleRange": IntParameter("Speckle range (max diff within a window)", 32, 0, 64),
+            "Disp12MaxDiff": IntParameter("Maximum allowable difference in the right-left check", 1, 0, 64),
+            "PreFilterCap": IntParameter("Max pre-filter output", 31, 1, 63),
+            "PreFilterSize": IntParameter("Pre-filter size (odd)", 9, 5, 255, to_valid=odd_only),
+        })
 
     def compute_disparity(self, left_image: np.ndarray, right_image: np.ndarray) -> np.ndarray:
         block_size = self.parameters['Block Size'].value
@@ -44,23 +79,63 @@ class StereoBMMethod(StereoMethod):
             block_size += 1
         stereoBM = cv2.StereoBM_create(numDisparities=self.parameters['Num Disparities'].value, 
                                        blockSize=block_size)
+        stereoBM.setTextureThreshold(self.parameters['TextureThreshold'].value)
+        stereoBM.setUniquenessRatio(self.parameters['Uniqueness Ratio'].value)
+        stereoBM.setSpeckleWindowSize(self.parameters['SpeckleWindowSize'].value)
+        stereoBM.setSpeckleRange(self.parameters['SpeckleRange'].value)
+        stereoBM.setDisp12MaxDiff(self.parameters['Disp12MaxDiff'].value)
+        stereoBM.setPreFilterCap(self.parameters['PreFilterCap'].value)
+        stereoBM.setPreFilterSize(self.parameters['PreFilterSize'].value)
+
         gray_left = cv2.cvtColor(left_image, cv2.COLOR_BGR2GRAY, left_image)
         gray_right = cv2.cvtColor(right_image, cv2.COLOR_BGR2GRAY, right_image)
-        disparity = stereoBM.compute(gray_left, gray_right)
+        # OpenCV returns 16x the disparity in pixels
+        disparity = stereoBM.compute(gray_left, gray_right) / np.float32(16.0)
         return disparity
 
 class StereoSGBMMethod(StereoMethod):
     def __init__(self):
-        params = {
-            "Num Disparities": IntParameter("Number of disparities", 64, 2, 256),
-            "Block Size": IntParameter("Kernel size for block matching", 3, 3, 21),
-        }
-        super().__init__("SGBM", "OpenCV Semi-Global Block Matching", params)
+        super().__init__("OpenCV SGBM", "OpenCV Semi-Global Block Matching", {})
+        self.reset_defaults ()
+
+    def reset_defaults(self):
+        nchannels = 1
+        # For more details:
+        # https://learnopencv.com/depth-perception-using-stereo-camera-python-c/
+        self.parameters.update ({
+            "Num Disparities": IntParameter("Number of disparities (pixels)", 64, 2, 256),
+            "Block Size": IntParameter("Kernel size for block matching (odd)", 3, 3, 63, to_valid=odd_only),
+            
+            "Mode": EnumParameter("Set it to StereoSGBM::MODE_HH to run the full-scale two-pass dynamic programming algorithm. It will consume O(W*H*numDisparities) bytes, which is large for 640x480 stereo and huge for HD-size pictures. By default, it is set to false .",
+                                  0, ["MODE_SGBM", "MODE_HH", "MODE_SGBM_3WAY", "MODE_HH4"]),
+            
+            
+            "P1": IntParameter("Penalty Cost (default=8*NChannels*BlockSize)", 8*nchannels*3*3, 0, 2000),
+            "P2": IntParameter("Penalty Cost. Must be > P1 (default=32*NChannels*BlockSize).", 32*nchannels*3*3, 0, 2000),
+            
+            "Uniqueness Ratio": IntParameter("How unique the match each for each pixel", 10, 0, 100),
+            "SpeckleWindowSize": IntParameter("Speckle window size in pixels (filter small objects). 0 to disable.", 100, 0, 1000),
+            "SpeckleRange": IntParameter("Speckle range (max diff within a window)", 32, 0, 64),
+            
+            "Disp12MaxDiff": IntParameter("Maximum allowable difference in the right-left check", 1, 0, 64),
+            "PreFilterCap": IntParameter("Max pre-filter output", 63, 1, 128),
+        })
 
     def compute_disparity(self, left_image: np.ndarray, right_image: np.ndarray) -> np.ndarray:
         stereoSGBM = cv2.StereoSGBM_create(numDisparities=self.parameters['Num Disparities'].value, 
                                          blockSize=self.parameters['Block Size'].value)
+
+        stereoSGBM.setMode(self.parameters['Mode'].index)
+        stereoSGBM.setP1(self.parameters['P1'].value)
+        stereoSGBM.setP2(self.parameters['P2'].value)
+        stereoSGBM.setPreFilterCap(self.parameters['PreFilterCap'].value)
+        stereoSGBM.setUniquenessRatio(self.parameters['Uniqueness Ratio'].value)
+        stereoSGBM.setSpeckleWindowSize(self.parameters['SpeckleWindowSize'].value)
+        stereoSGBM.setSpeckleRange(self.parameters['SpeckleRange'].value)
+        stereoSGBM.setDisp12MaxDiff(self.parameters['Disp12MaxDiff'].value)
+
         gray_left = cv2.cvtColor(left_image, cv2.COLOR_BGR2GRAY, left_image)
         gray_right = cv2.cvtColor(right_image, cv2.COLOR_BGR2GRAY, right_image)
-        disparity = stereoSGBM.compute(gray_left, gray_right)
+        # OpenCV returns 16x the disparity in pixels
+        disparity = stereoSGBM.compute(gray_left, gray_right) / np.float32(16.0)
         return disparity
--- stereodemo/visualizer.py
+++ stereodemo/visualizer.py
@@ -12,7 +12,7 @@ import open3d as o3d
 import open3d.visualization.gui as gui
 import open3d.visualization.rendering as rendering
 
-from .methods import IntParameter, StereoMethod
+from .methods import IntParameter, EnumParameter, StereoMethod
 
 @dataclass
 class Calibration:
@@ -48,16 +48,11 @@ class Visualizer:
 
         self.vis = gui.Application.instance
 
-        self.set_input (None, None, None)
         self.stereo_methods = stereo_methods
         self.stereo_methods_output = {}
+        self.set_input (None, None, None)
 
-        for name in self.stereo_methods.keys():
-            self.stereo_methods_output[name] = MethodOutput(
-                disparity_pixels=None,
-                computation_time=np.nan,
-                point_cloud = o3d.t.geometry.PointCloud()
-            )        
+        self._clear_outputs ()
 
         self.window = gui.Application.instance.create_window("Stereo Demo", 1024, 768)
         w = self.window  # to make the code more concise
@@ -67,30 +62,22 @@ class Visualizer:
         # 3D widget
         self._scene = gui.SceneWidget()
         self._scene.scene = rendering.Open3DScene(w.renderer)
-        self._scene.scene.show_ground_plane(True, rendering.Scene.GroundPlane.XZ)
+        # self._scene.scene.show_ground_plane(True, rendering.Scene.GroundPlane.XZ)
         self._scene.set_view_controls(gui.SceneWidget.Controls.ROTATE_CAMERA)
-
-        eye = np.array([0, 0, 0])
-        lookat = np.array([0, 0.28, 1.0])
-        up = np.array([0, 1, 0])
-        self._scene.look_at(eye, lookat, up)
 
         for name, o in self.stereo_methods_output.items():
             self._scene.scene.add_geometry(name, o.point_cloud, rendering.MaterialRecord())
 
+        self._reset_camera()
+
         em = w.theme.font_size
-        separation_height = int(round(0.5 * em))
+        self.separation_height = int(round(0.5 * em))
         self._settings_panel = gui.Vert(0, gui.Margins(0.25 * em, 0.25 * em, 0.25 * em, 0.25 * em))
 
-        view_ctrls = gui.CollapsableVert("View controls", 0.25 * em, gui.Margins(em, 0, 0, 0))
         self._next_image_button = gui.Button("Next Image")
         self._next_image_button.set_on_clicked(self._next_image_clicked)
-        view_ctrls.add_child(self._next_image_button)
-        self._show_axes = gui.Checkbox("Show axes")
-        self._show_axes.set_on_checked(self._on_show_axes)
-        view_ctrls.add_child(self._show_axes)
-        
-        self._settings_panel.add_child(view_ctrls)
+        self._settings_panel.add_child(self._next_image_button)
+        self._settings_panel.add_fixed(self.separation_height)
 
         self.algo_list = gui.ListView()
         self.algo_list.set_items(list(stereo_methods.keys()))
@@ -104,6 +91,16 @@ class Visualizer:
 
         self.last_runtime = gui.Label("")
         self._settings_panel.add_child (self.last_runtime)
+
+        view_ctrls = gui.CollapsableVert("View controls", 0.25 * em, gui.Margins(em, 0, 0, 0))
+        reset_cam_button = gui.Button("Reset Camera")
+        reset_cam_button.set_on_clicked(self._reset_camera)
+        view_ctrls.add_child(reset_cam_button)
+        self._show_axes = gui.Checkbox("Show axes")
+        self._show_axes.set_on_checked(self._on_show_axes)
+        view_ctrls.add_child(self._show_axes)
+        self._settings_panel.add_fixed(self.separation_height)
+        self._settings_panel.add_child(view_ctrls)
 
         w.set_on_layout(self._on_layout)
         w.add_child(self._scene)
@@ -124,27 +121,95 @@ class Visualizer:
                                                                         cx=self.calibration.cx,
                                                                         cy=self.calibration.cy)
 
+            self._clear_outputs ()
+            self._run_current_method ()
+
     def update_once (self):
         return gui.Application.instance.run_one_tick()
+
+    def _clear_outputs (self):
+        for name in self.stereo_methods.keys():
+            self.stereo_methods_output[name] = MethodOutput(
+                disparity_pixels=None,
+                computation_time=np.nan,
+                point_cloud = o3d.t.geometry.PointCloud()
+            )
+
+    def _reset_camera (self):
+        # bbox = o3d.geometry.AxisAlignedBoundingBox(np.array([-10, 0,-10]), np.array([0,3,0]))
+        bbox = self._scene.scene.bounding_box
+        min_bound, max_bound = bbox.min_bound.copy(), bbox.max_bound.copy()
+        min_bound[0] = min(min_bound[0], -5)
+        min_bound[2] = min(min_bound[2], -5)
+        max_bound[0] = max(max_bound[0],  5)
+        max_bound[1] = max(max_bound[1],  2)
+        max_bound[2] = 0
+        bbox.min_bound, bbox.max_bound = min_bound, max_bound
+
+        self._scene.setup_camera(60.0, bbox, np.array([0,0,0]))
+        eye = np.array([0, 0.5,  1.0])
+        lookat = np.array([0, 0, -1.0])
+        up = np.array([0, 1.0, 0])
+        self._scene.look_at(lookat, eye, up)
 
     def _build_stereo_method_widgets(self, name):
         em = self.window.theme.font_size
         method = self.stereo_methods[name]
         container = gui.Vert(0, gui.Margins(0.25 * em, 0.25 * em, 0.25 * em, 0.25 * em))
+        label = gui.Label(method.description)
+        label.text_color = gui.Color(1.0, 0.5, 0.0)
+        container.add_child(label)
+        self._reload_settings_functions = []
         for name, param in method.parameters.items():
             if isinstance(param, IntParameter):
                 horiz = gui.Horiz(0, gui.Margins(0.25 * em, 0.25 * em, 0.25 * em, 0.25 * em))
-                horiz.add_child(gui.Label(name))
+                label = gui.Label(name)
+                label.tooltip = param.description
+                horiz.add_child(label)
                 slider = gui.Slider(gui.Slider.INT)
                 slider.set_limits(param.min, param.max)
                 slider.int_value = param.value
-                callback = lambda value, param=param: setattr(param, 'value', int(value))
+                def set_value_from_method(slider=slider, method=method, name=name):
+                    print (f"Setting value of {slider} to {method.parameters[name].value}")
+                    slider.int_value = method.parameters[name].value
+                self._reload_settings_functions.append(set_value_from_method)
+                # workaround late binding
+                # https://docs.python-guide.org/writing/gotchas/#:~:text=Python's%20closures%20are%20late%20binding,surrounding%20scope%20at%20call%20time.
+                def callback(value, method=method, name=name, slider=slider):
+                    p = method.parameters[name]
+                    p.set_value(int(value))
+                    slider.int_value = p.value
                 slider.set_on_value_changed(callback)
                 horiz.add_child(slider)
                 container.add_child(horiz)
-            apply_button = gui.Button("Run")
-            apply_button.set_on_clicked(self._apply_method_clicked)
-        container.add_child(apply_button)
+            elif isinstance(param, EnumParameter):
+                horiz = gui.Horiz(0, gui.Margins(0.25 * em, 0.25 * em, 0.25 * em, 0.25 * em))
+                label = gui.Label(name)
+                label.tooltip = param.description
+                horiz.add_child(label)
+                combo = gui.Combobox()
+                for value in param.values:
+                    combo.add_item(value)
+                combo.selected_index = param.index
+                def callback(combo_idx, combo_val, method=method, name=name, combo=combo):
+                    method.parameters[name].set_index(combo.selected_index)
+                combo.set_on_selection_changed(callback)
+                def set_value_from_method(combo=combo, method=method, name=name):
+                    combo.selected_index = method.parameters[name].index
+                self._reload_settings_functions.append(set_value_from_method)
+                horiz.add_child(combo)
+                container.add_child(horiz)
+            
+        horiz = gui.Horiz(0, gui.Margins(0.25 * em, 0.25 * em, 0.25 * em, 0.25 * em))        
+        apply_button = gui.Button("Run")
+        apply_button.horizontal_padding_em = 3
+        apply_button.set_on_clicked(self._run_current_method)
+        horiz.add_child(apply_button)
+        horiz.add_fixed(self.separation_height)
+        reset_default = gui.Button("Reset defaults")            
+        reset_default.set_on_clicked(self._reset_method_defaults)
+        horiz.add_child(reset_default)
+        container.add_child(horiz)
         return container
 
     def _on_algo_list_selected(self, name: str, is_dbl_click: bool):
@@ -156,6 +221,8 @@ class Visualizer:
             self._scene.scene.show_geometry(other_name, False)
         self._scene.scene.show_geometry(name, True)
         self._apply_settings()
+        if self.stereo_methods_output[name].disparity_pixels is None:
+            self._run_current_method ()
 
     def _on_show_axes(self, show):
         self.settings.show_axes = show
@@ -167,7 +234,14 @@ class Visualizer:
     def _apply_settings(self):
         self._scene.scene.show_axes(self.settings.show_axes)
 
-    def _apply_method_clicked(self):
+    def _reset_method_defaults(self):
+        name = self.algo_list.selected_value
+        method = self.stereo_methods[name]
+        method.reset_defaults()
+        for m in self._reload_settings_functions:
+            m()
+
+    def _run_current_method(self):
         if self.left_image is None:
             return
         name = self.algo_list.selected_value
@@ -179,7 +253,7 @@ class Visualizer:
         show_color_disparity (name, disparity)
         tend = time.time()
 
-        depth_meters = np.float32(16.0 * self.calibration.baseline_meters * self.calibration.fx) / disparity
+        depth_meters = np.float32(self.calibration.baseline_meters * self.calibration.fx) / disparity
         depth_meters = np.nan_to_num(depth_meters)
         depth_meters = np.clip (depth_meters, -1.0, 10.0)
 
@@ -189,7 +263,7 @@ class Visualizer:
         rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(o3d_left,
                                                                   o3d_depth,
                                                                   1,
-                                                                  depth_trunc=5.0,
+                                                                  depth_trunc=10.0,
                                                                   convert_rgb_to_intensity=False)
         output.point_cloud = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, self.o3dCameraIntrinsic)
         output.point_cloud.transform([[1,0,0,0],[0,-1,0,0],[0,0,-1,0],[0,0,0,1]])
@@ -206,18 +280,18 @@ class Visualizer:
         if np.isnan(output.computation_time):
             self.last_runtime.text = "No output yet."
         else:
-            self.last_runtime.text = f"Computation time for {name}: {output.computation_time*1e3:.1f} ms"        
+            self.last_runtime.text = f"Computation time: {output.computation_time*1e3:.1f} ms"
 
     def _on_layout(self, layout_context):
         # The on_layout callback should set the frame (position + size) of every
         # child correctly. After the callback is done the window will layout
         # the grandchildren.
+        settings_width = 17 * layout_context.theme.font_size
         r = self.window.content_rect
-        self._scene.frame = r
-        width = 17 * layout_context.theme.font_size
-        height = min(
-            r.height,
-            self._settings_panel.calc_preferred_size(
-                layout_context, gui.Widget.Constraints()).height)
-        self._settings_panel.frame = gui.Rect(r.get_right() - width, r.y, width,
-                                              height)
+        self._scene.frame = gui.Rect(0, r.y, r.get_right() - settings_width, r.height)
+        # height = min(
+        #     r.height,
+        #     self._settings_panel.calc_preferred_size(
+        #         layout_context, gui.Widget.Constraints()).height)
+        height = r.height
+        self._settings_panel.frame = gui.Rect(r.get_right() - settings_width, r.y, settings_width, height)
